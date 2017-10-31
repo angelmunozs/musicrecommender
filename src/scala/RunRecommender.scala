041c28bdb2a2f5d56e8e0985d73d2c0eb99a71d0
angelmunozs <angel.munoz.sagaseta@gmail.com>
2017-10-31 09:08:44 +0100

    Update directories in Scala script

--- src/scala/RunRecommender.scala
+++ src/scala/RunRecommender.scala
@@ -19,9 +19,9 @@ object RunRecommender {
   def main(args: Array[String]): Unit = {
     val spark = SparkSession.builder().getOrCreate()
     // Optional, but may help avoid errors due to long lineage
-    spark.sparkContext.setCheckpointDir("hdfs:///tmp/")
+    spark.sparkContext.setCheckpointDir("../../tmp/")
 
-    val base = "hdfs:///user/ds/"
+    val base = "../../data"
     val rawUserArtistData = spark.read.textFile(base + "user_artist_data.txt")
     val rawArtistData = spark.read.textFile(base + "artist_data.txt")
     val rawArtistAlias = spark.read.textFile(base + "artist_alias.txt")

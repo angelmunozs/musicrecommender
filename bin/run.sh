bb28e2c4fed572f1fae801c8c68bbf70b61de9e5
angelmunozs <angel.munoz.sagaseta@gmail.com>
2017-12-31 12:16:02 +0000

    Change Master IP dynamically from Dockerfile

--- bin/run.sh
+++ bin/run.sh
@@ -1,4 +1,8 @@
 # 1. Lanuch spark from docker-compose.yml
 docker-compose up -d
-# 2. Launch recommender from custom Dockerfile
+# 2. Generate Dockerfile with master IP
+MASTER_IP=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' musicrecommender_master_1)
+cp Dockerfile.dist Dockerfile
+find Dockerfile -type f -exec sed -i s/%MASTER_IP%/$MASTER_IP/g {} \;
+# 3. Launch recommender from custom Dockerfile
 docker build -t musicrecommender .

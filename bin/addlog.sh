58652ce2460905442b395366426626249431bca9
angelmunozs <angel.munoz.sagaseta@gmail.com>
2018-01-09 10:00:06 +0000

    Update script addlog.sh

--- bin/addlog.sh
+++ bin/addlog.sh
@@ -1,28 +1,72 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
-# New user ID to append to the dataset
-NEW_USER_ID=2443549
+# New user IDs to append to the dataset
+ID_ANGEL=1
+ID_DANI=2
+ID_ALBERTO=3
 
-# Append new lines to dataset
-echo "$NEW_USER_ID 6672292 11" >> data/user_artist_data.txt # Run DMC
-echo "$NEW_USER_ID 10488786 42" >> data/user_artist_data.txt # System of a Down
-echo "$NEW_USER_ID 1322292 34" >> data/user_artist_data.txt # Notorious BIG
-echo "$NEW_USER_ID 9918181 33" >> data/user_artist_data.txt # Mobb Deep
-echo "$NEW_USER_ID 1303150 19" >> data/user_artist_data.txt # Big L
-echo "$NEW_USER_ID 6872821 34" >> data/user_artist_data.txt # KRS One
-echo "$NEW_USER_ID 10485580 2" >> data/user_artist_data.txt # Tupac
-echo "$NEW_USER_ID 9925342 9" >> data/user_artist_data.txt # Das EFX
-echo "$NEW_USER_ID 10057146 56" >> data/user_artist_data.txt # System of a Down
-echo "$NEW_USER_ID 1296254 35" >> data/user_artist_data.txt # Black Sabbath
-echo "$NEW_USER_ID 10099365 78" >> data/user_artist_data.txt # Ramones
-echo "$NEW_USER_ID 1042694 34" >> data/user_artist_data.txt # Lords of the Underground
-echo "$NEW_USER_ID 1144985 12" >> data/user_artist_data.txt # Nas
-echo "$NEW_USER_ID 1116861 40" >> data/user_artist_data.txt # Onyx
-echo "$NEW_USER_ID 1246987 40" >> data/user_artist_data.txt # Sex Pistols
-echo "$NEW_USER_ID 1332399 11" >> data/user_artist_data.txt # House of Pain
-echo "$NEW_USER_ID 9934967 9" >> data/user_artist_data.txt # Black Moon
-echo "$NEW_USER_ID 9972003 28" >> data/user_artist_data.txt # Mad Flava
-echo "$NEW_USER_ID 10476272 31" >> data/user_artist_data.txt # Cypress Hill
-echo "$NEW_USER_ID 1246662 65" >> data/user_artist_data.txt # Wu-Tang Clan
-echo "$NEW_USER_ID 1191430 14" >> data/user_artist_data.txt # Eazy-E
-echo "$NEW_USER_ID 1080345 2" >> data/user_artist_data.txt # Da Bush Babees
+# Append new lines to dataset for Angel
+echo "$ID_ANGEL 2170648 11" >> data/user_artist_data.txt # Run DMC
+echo "$ID_ANGEL 1128384 56" >> data/user_artist_data.txt # System of a Down
+echo "$ID_ANGEL 1192119 34" >> data/user_artist_data.txt # Notorious BIG
+echo "$ID_ANGEL 1112115 33" >> data/user_artist_data.txt # Mobb Deep
+echo "$ID_ANGEL 9918430 19" >> data/user_artist_data.txt # Big L
+echo "$ID_ANGEL 6872821 34" >> data/user_artist_data.txt # KRS One
+echo "$ID_ANGEL 10485580 12" >> data/user_artist_data.txt # Tupac
+echo "$ID_ANGEL 9925342 9" >> data/user_artist_data.txt # Das EFX
+echo "$ID_ANGEL 1296254 35" >> data/user_artist_data.txt # Black Sabbath
+echo "$ID_ANGEL 1150860 78" >> data/user_artist_data.txt # Ramones
+echo "$ID_ANGEL 1042694 34" >> data/user_artist_data.txt # Lords of the Underground
+echo "$ID_ANGEL 1244487 12" >> data/user_artist_data.txt # Nas
+echo "$ID_ANGEL 1116861 40" >> data/user_artist_data.txt # Onyx
+echo "$ID_ANGEL 1129610 40" >> data/user_artist_data.txt # Sex Pistols
+echo "$ID_ANGEL 1088253 11" >> data/user_artist_data.txt # House of Pain
+echo "$ID_ANGEL 9934967 9" >> data/user_artist_data.txt # Black Moon
+echo "$ID_ANGEL 9972003 28" >> data/user_artist_data.txt # Mad Flava
+echo "$ID_ANGEL 10476272 31" >> data/user_artist_data.txt # Cypress Hill
+echo "$ID_ANGEL 6871192 65" >> data/user_artist_data.txt # Wu-Tang Clan
+echo "$ID_ANGEL 1191430 14" >> data/user_artist_data.txt # Eazy-E
+echo "$ID_ANGEL 1080345 2" >> data/user_artist_data.txt # Da Bush Babees
+
+# Append new lines to dataset for Dani
+echo "$ID_DANI 10057146 20" >> data/user_artist_data.txt # System of a Down
+echo "$ID_DANI 2040188 50" >> data/user_artist_data.txt # Blink-182
+echo "$ID_DANI 1322284 55" >> data/user_artist_data.txt # Green Day
+echo "$ID_DANI 10594015 40" >> data/user_artist_data.txt # Sum 41
+echo "$ID_DANI 1245959 35" >> data/user_artist_data.txt # Bon Jovi
+echo "$ID_DANI 10151469 25" >> data/user_artist_data.txt # Kid Rock
+echo "$ID_DANI 2047285 15" >> data/user_artist_data.txt # The Who
+echo "$ID_DANI 10300106 15 " >> data/user_artist_data.txt # The Beatles
+echo "$ID_DANI 1182039 5" >> data/user_artist_data.txt # Deadmau5
+echo "$ID_DANI 10443779 10 " >> data/user_artist_data.txt # The Rasmus
+echo "$ID_DANI 3111 30" >> data/user_artist_data.txt # Simple Plan
+echo "$ID_DANI 6962617 10" >> data/user_artist_data.txt # Pitbull
+echo "$ID_DANI 1336494 10" >> data/user_artist_data.txt # David Guetta
+echo "$ID_DANI 10752908 10" >> data/user_artist_data.txt # Tinie Tempah
+echo "$ID_DANI 1060155 30" >> data/user_artist_data.txt # Estopa
+echo "$ID_DANI 1252153 20" >> data/user_artist_data.txt # Cafe Quijano
+echo "$ID_DANI 1246321 25" >> data/user_artist_data.txt # Black Eyed Peas
+echo "$ID_DANI 1246140 25" >> data/user_artist_data.txt # Depeche Mode
+echo "$ID_DANI 7004690 60" >> data/user_artist_data.txt # Linkin Park
+
+# Append new lines to dataset for Alberto
+echo "$ID_ALBERTO 1032435 35" >> data/user_artist_data.txt # BONEY M 
+echo "$ID_ALBERTO 1117252 40" >> data/user_artist_data.txt # TIERRA SANTA 
+echo "$ID_ALBERTO 1303124 55" >> data/user_artist_data.txt # MODERN TALKING 
+echo "$ID_ALBERTO 10477256 50" >> data/user_artist_data.txt # ERIC CLAPTON 
+echo "$ID_ALBERTO 6937594 30" >> data/user_artist_data.txt # HEROES DEL SILENCIO 
+echo "$ID_ALBERTO 6909956 10" >> data/user_artist_data.txt # JUDAS PRIEST 
+echo "$ID_ALBERTO 6979332 15" >> data/user_artist_data.txt # MANA 
+echo "$ID_ALBERTO 1000412 10" >> data/user_artist_data.txt # JOHN DENVER 
+echo "$ID_ALBERTO 1098935 35" >> data/user_artist_data.txt # U2 
+echo "$ID_ALBERTO 1142200 25" >> data/user_artist_data.txt # COLDPLAY 
+echo "$ID_ALBERTO 6945944 15" >> data/user_artist_data.txt # METALLICA 
+echo "$ID_ALBERTO 1241157 10" >> data/user_artist_data.txt # MADONNA 
+echo "$ID_ALBERTO 10480481 15" >> data/user_artist_data.txt # ABBA 
+echo "$ID_ALBERTO 1309649 20" >> data/user_artist_data.txt # THE CORRS 
+echo "$ID_ALBERTO 1089171 10" >> data/user_artist_data.txt # OASIS 
+echo "$ID_ALBERTO 1072203 25" >> data/user_artist_data.txt # GREEN DAY 
+echo "$ID_ALBERTO 1023007 40" >> data/user_artist_data.txt # MAROON 5 
+echo "$ID_ALBERTO 1022831 20" >> data/user_artist_data.txt # DUNCAN DHU 
+echo "$ID_ALBERTO 1060529 45" >> data/user_artist_data.txt # QUEEN 
+echo "$ID_ALBERTO 6760165 15" >> data/user_artist_data.txt # THE CRANBERRIES 
